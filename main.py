788110c27900ec9c2a81ac49499f6991d279e790
unknown <dhananjanarathnayake123@gmail.com>
2024-12-18 14:02:49 +0530

    CORS modified

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ app = FastAPI()
 # CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
